c1c744c5bffcd6820294630eff3c322f6783e2d4
Aryan Rangapur <154872205+aryanrangapur@users.noreply.github.com>
2025-10-23 13:18:51 +0530

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://expenso-jxyq.onrender.com/api/api/",
+  baseURL: "https://expenso-jxyq.onrender.com/api/",
 })
 
 api.interceptors.request.use((config) => {
